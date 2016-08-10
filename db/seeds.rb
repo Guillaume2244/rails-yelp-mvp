80e7631f6172faaed2d7639c8f3fec88f6b444d7
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-10 11:31:43 +0200

    add some validations to restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "0740987654",
+    category:      "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "074087657654",
+    category:      "belgian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "0740987097655554",
+    category:      "italian"
+  }
+]
