c407e7f688c03e52f32a48c4e6918b12f1d9b8f6
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-10 11:52:10 +0200

    inclusion rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
 
   validates :restaurant, presence: true
   validates :content, presence: true
-  validates :rating, inclusion: { in: [0..5], allow_nil: false }
+  validates :rating, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }
 
   belongs_to :restaurant, dependent: :destroy
 end
