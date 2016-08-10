eed205fbba268ec4346a21c171959b1f53a40fdb
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-10 11:34:08 +0200

    add another validations to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
+  validates :stars, inclusion: { in: [1..3], allow_nil: false }
 
 end
