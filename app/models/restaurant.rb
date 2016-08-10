084327f24916a37a85f9782cbac1a07b41e0ee35
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-10 11:51:04 +0200

    some modifs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,6 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
 end
